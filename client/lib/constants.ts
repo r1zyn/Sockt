be8ccb76c1b4465b1e3c6d78eab3b79a0adefc23
Ascendus <ascendus.dev@gmail.com>
2022-08-21 19:23:39 +1200

    feat: updated metadata

--- client/lib/constants.ts
+++ client/lib/constants.ts
@@ -1,9 +1,9 @@
 import { MetaDataOptions } from "./types";
 
 export const metadata: MetaDataOptions = {
-	url: process.env.NODE_ENV === "production" ? "" : "http:/localhost:3000/",
+	url: process.env.NODE_ENV === "production" ? "https://sockt.vercel.app" : "http:/localhost:3000/",
 	"theme-color": "#2f3136",
-	image: "/assets/ARGW6gw7BJSK4zBLysS1vN2E1eRvKM3n.png",
+	image: "/assets/065c2e59-ac45-4056-b562-1c279d15f101.png",
 	icon: "/assets/ARGW6gw7BJSK4zBLysS1vN2E1eRvKM3n.png"
 };
 
